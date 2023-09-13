4e05783e6edcdc83400822aabaaedee1af3bc7be
Yusen Hu <richardyusen@gmail.com>
2023-09-12 21:52:15 -0400

    Working + Docs

--- pats.py
+++ pats.py
@@ -1,15 +1,67 @@
 
+
+# Configuration file for fpdf
+
+# Do not change the names of existing variables; some of them
+# are used by fpdf internally. Add new variables as needed.
+
+# Python regex syntax documentation:
+# https://docs.python.org/3/library/re.html#regular-expression-syntax
+
+
+# The `fmt` function allows you to reuse regex subpatterns.
+# It will replace any occurence of {x} in the input string
+# with the value of the kwarg whose key is x. Essentially,
+# this is a more restrictive version of Python's built-in
+# str.format() method. Usage examples are given below.
 from utils import fmt
 
-start_of_ref_list = r'^ *(References|REFERENCES|Bibliography) *$'
+
+# This searches for the point in the PDF where fpdf will
+# start looking for reference destinations (i.e. where in-text
+# citations will link to)
+start_of_ref_list = r'^(?:References|REFERENCES|Bibliography|BIBLIOGRAPHY) ?$'
+
+
+# Define regex subpatterns here. The convention is to make
+# any groups in subpatterns non-capturing and explicitly wrap
+# groups in brackets in the top-level pattern if needed. This
+# is to avoid confusion on how many capturing groups are in
+# a top-level pattern.
 valid_year = r'(?:19[5-9]\d|20[0-4]\d|2050)[a-z]?'
-pos_int = r'[1-9][0-9]*'
+lastname = r'(?:[a-zA-Z]{2,}(?:-[a-zA-Z]{2,})*)'
 
-# I'm sorry. Fuck those whose lastnames contain spaces. They're impossible to parse.
-#badlastname = r'([a-zA-Z?]{2,}(?:(?:\s|-)[a-zA-Z?]{2,})*)'
-lastname = r'([a-zA-Z?]{2,}(?:-[a-zA-Z?]{2,})*)'
 
-ieee_ref = fmt(r'({i})', i=pos_int)
+# A note on capture groups
+
+# They are used directly by fpdf as the keys for linking references.
+# For instance, the IEEE style regex captures the positive integer
+# inside the square brackets (e.g. [1], [2,3,4], etc.) and uses it
+# as the citation key, so an in-text citation like [6] will link to
+# the entry marked as [6] in the reference list.
+
+# The APA style is a little more involved. In-text citations are of
+# the form (Author, Year), where Author can be:
+
+#   1. One last name, e.g. Smith
+#   2. Two last names, e.g. Smith and Jones
+#   3. One last name followed by `et al.`, e.g. Smith et al.
+
+# The Year can also have a letter suffix, e.g. 2019a, 2015b, etc. to
+# denote different works published by the same author in the same year.
+
+# As a result, we have to define three capturing groups: two for the
+# author names (the 2nd may be None), and one for the year.
+
+# The APA style also have two in-text citation styles:
+#   1. (Author1, Year1; Author2, Year2; ...)
+#   2. Author1 (Year1)
+# which we refer to as 'refs' and 'alt-ref' respectively
+# in the dictionary below.
+
+
+# IEEE style. Commonly used in computer science.
+ieee_ref = '([1-9][0-9]*)'
 ieee = {
     'ref': ieee_ref,
     'alt-ref': None,
@@ -17,14 +69,24 @@ ieee = {
     'dst': fmt(r'^\[{r}\] ', r=ieee_ref),
 }
 
-# One capitalized word, comma, any spaces, valid year.
-# Capture groups: author name and year.
-apa_ref = fmt(r'{l}(?:|\set\sal\.|\sand\s{l})'
+
+# APA style.
+# Known limitations:
+#
+#   1. In-text citations split across pages with interjecting
+#      text are not recognized. Example: bottom of page 35
+#      of apa-std-1.pdf
+#
+#   2. Last names containing spaces are not recognized.
+#      Example: 'van den Oord et al., 2017' (page 37 of
+#      apa-std-1.pdf)
+#
+apa_ref = fmt(r'({l})(?:|\set\sal\.|\sand\s({l}))'
               r',\s({y})', l=lastname, y=valid_year)
 apa = {
     'ref': apa_ref,
 
-    'alt-ref': fmt(r'{l}(?:|\set\sal\.|\sand\s{l})'
+    'alt-ref': fmt(r'({l})(?:|\set\sal\.|\sand\s({l}))'
                    r'\s\(({y})\)', l=lastname, y=valid_year),
 
     # Any number of semicolon-separated refs,
@@ -35,11 +97,16 @@ apa = {
     # comma, any number of characters (including newline), period, any spaces,
     # open parentheses, valid year, close parentheses, period.
     # Capture groups: author name and year.
-    'dst': fmt(r'^{l},(?:(?:\s|-)[A-Z]\.)+'
-               r'(?:|,\s.*?\.|\sand\s{l},(?:(?:\s|-)[A-Z]\.)+)'
+    'dst': fmt(r'^({l}),(?:(?:\s|-)[A-Z]\.)+'
+               r'(?:|,\s.*?\.|\sand\s({l}),(?:(?:\s|-)[A-Z]\.)+)'
                r'\s\(({y})\)\.\s', l=lastname, y=valid_year),
 }
 
+
+# Define your own styles here...
+
+
+# Add your styles to this dict, so fpdf can use them.
 patterns = {
     'ieee': ieee,
     'apa': apa,
--- utils.py
+++ utils.py
@@ -5,38 +5,56 @@ from unidecode import unidecode
 import warnings
 warnings.filterwarnings('ignore', module='unidecode')
 
+
 def fmt(s: str, **kwargs):
     for k in kwargs.keys():
         s = s.replace('{' + k + '}', kwargs[k])
     return s
 
+
+# Return the PDF text content as a single string, and
+# a mapping from character position to page number, 
+# bounding box, and its containing fitz span.
+
 def parse(pdf: fitz.Document) -> (str, dict):
-    s, idx, d = [], 0, {}
+
+    s, d, idx = [], {}, 0
+
     for page in pdf.pages():
+
         tdict = page.get_text(
             'rawdict',
-            flags=fitz.TEXTFLAGS_RAWDICT & ~fitz.TEXT_PRESERVE_IMAGES
+            flags =  fitz.TEXTFLAGS_RAWDICT
+                  & ~fitz.TEXT_PRESERVE_IMAGES
         )
+
         for block in tdict['blocks']:
             for line in block['lines']:
                 for span in line['spans']:
                     for char in span['chars']:
                         d[idx] = (page.number, *char['bbox'], span)
-                        c = char['c']
-                        uc = unidecode(c)
-                        s.append(uc)
-                        idx += len(uc)
+                        c = unidecode(char['c'])
+                        s.append(c)
+                        idx += len(c)
+
                 prev_span = line['spans'][-1]
                 prev_char = prev_span['chars'][-1]
+
+                # Separate each line by '\n'.
+                # Remove line-breaking hyphens if needed.
                 if prev_char['c'] == '-':
-                    for i in range(-1, -50, -1):
-                        if s[i] == ' ':
+                    space_found = False
+                    for i in range(-1, -100, -1):
+                        if str.isspace(s[i]):
                             s[i] = '\n'
+                            space_found = True
                             break
+                    assert(space_found)
                     s.pop()
                     idx -= 1
                 else:
                     d[idx] = (page.number, *prev_char['bbox'], prev_span)
                     s.append('\n')
                     idx += 1
+
     return ''.join(s), d
